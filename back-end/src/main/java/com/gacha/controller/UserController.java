f9407389fae9a071018633b16834c09a1e2fc475
parksangchan <gkgk00142@naver.com>
2025-05-07 19:34:26 +0900

    [BE/user] controller 요청 맵핑 설정

--- back-end/src/main/java/com/gacha/controller/UserController.java
+++ back-end/src/main/java/com/gacha/controller/UserController.java
@@ -1,5 +1,19 @@
 package com.gacha.controller;
 
-public class UserController {
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.gacha.model.dto.request.RegistRequest;
+import com.gacha.model.dto.response.RegistResponse;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    @PostMapping("/regist")
+    public ResponseEntity<?> regist(@ModelAttribute RegistRequest registRequest){
+        return ResponseEntity.ok().body(new RegistResponse("ok", "200", "가입 성공", new Object()));
+    }
 }
