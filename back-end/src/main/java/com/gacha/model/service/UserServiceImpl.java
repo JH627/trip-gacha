fc0d98773ce520b16cf39e8bb469064bc9c8ac77
parksangchan <gkgk00142@naver.com>
2025-05-10 19:37:11 +0900

    ✨ [feat] : 비밀번호 해싱 기능 추가

--- back-end/src/main/java/com/gacha/model/service/UserServiceImpl.java
+++ back-end/src/main/java/com/gacha/model/service/UserServiceImpl.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 
 import com.gacha.model.dao.UserDao;
 import com.gacha.model.dto.request.RegistRequest;
+import com.gacha.util.PasswordUtil;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -15,14 +16,16 @@ public class UserServiceImpl implements UserService {
     @Override
     public boolean regist(RegistRequest registRequest) {
         String email = registRequest.getEmail();
-        // TODO : 암호를 해시화
+
         String password = registRequest.getPassword();
+        String hashingPassword = PasswordUtil.hash(password);
+
         String nickname = registRequest.getNickname();
         // TODO : 이미지 파일 S3에 올리기 -> 반환 url 저장
         String profileImg = "";
 
         try{
-            userDao.regist(email, password, nickname, profileImg);
+            userDao.regist(email, hashingPassword, nickname, profileImg);
         } catch(DuplicateKeyException e){
             System.out.println("중복된 사용자 이메일");
             return false;
---
+++ back-end/src/main/java/com/gacha/util/PasswordUtil.java
@@ -0,0 +1,12 @@
+package com.gacha.util;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+public class PasswordUtil {
+    private static final PasswordEncoder encoder = new BCryptPasswordEncoder();
+
+    public static String hash(String rawPassword) {
+        return encoder.encode(rawPassword);
+    }
+}
