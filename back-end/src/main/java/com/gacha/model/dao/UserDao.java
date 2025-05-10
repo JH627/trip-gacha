ff7bff762db468da248a16cc68f56f6592a84550
parksangchan <gkgk00142@naver.com>
2025-05-10 19:30:46 +0900

    ✨ [feat] : 회원 가입 SQL mapper와 dao 연결

--- back-end/src/main/java/com/gacha/model/dao/UserDao.java
+++ back-end/src/main/java/com/gacha/model/dao/UserDao.java
@@ -1,5 +1,8 @@
 package com.gacha.model.dao;
 
-public interface UserDao {
+import org.apache.ibatis.annotations.Mapper;
 
+@Mapper
+public interface UserDao {
+    void regist(String email, String password, String nickname, String profile_img);
 }
