923a65df5a3ce07a0cf9b59c7fe0bd1a9181ea55
parksangchan <gkgk00142@naver.com>
2025-05-10 18:42:20 +0900

    ✨ [feat] : 이메일 인증 코드 확인 요청 처리 함수 추가

--- back-end/src/main/java/com/gacha/controller/MailController.java
+++ back-end/src/main/java/com/gacha/controller/MailController.java
@@ -1,9 +1,12 @@
 package com.gacha.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.gacha.model.dto.request.EmailVerificationConfirmRequest;
 import com.gacha.model.dto.request.EmailVerificationRequest;
 import com.gacha.model.dto.response.Response;
 import com.gacha.model.service.MailService;
@@ -23,5 +26,17 @@ public class MailController {
         mailService.sendSimpleMessage(body.getEmail());
 
         return Response.onSuccess();
+    }
+
+    @PostMapping("/verification-confirm")
+    public ResponseEntity<Response<?>> checkVerifyCode(@RequestBody EmailVerificationConfirmRequest body) throws Exception {
+        // TODO : 이메일 형식인지 체킹
+        if (!mailService.verifyCode(body.getEmail(), body.getCode())) {
+            return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(Response.onFailure(HttpStatus.BAD_REQUEST, "400", "일치하지 않음", null));
+        } else {
+            return ResponseEntity.ok(Response.onSuccess());
+        }
     }
 }
