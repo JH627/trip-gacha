48af81e43e2ff6aa5c751bded381917ce17c5ad4
parksangchan <gkgk00142@naver.com>
2025-05-10 19:30:27 +0900

    ✨ [feat] : 회원 가입 기능 로직 구현

--- back-end/src/main/java/com/gacha/model/service/UserService.java
+++ back-end/src/main/java/com/gacha/model/service/UserService.java
@@ -1,6 +1,7 @@
 package com.gacha.model.service;
 
+import com.gacha.model.dto.request.RegistRequest;
+
 public interface UserService {
-	
-	
+	boolean regist(RegistRequest registRequest);
 }
--- back-end/src/main/java/com/gacha/model/service/UserServiceImpl.java
+++ back-end/src/main/java/com/gacha/model/service/UserServiceImpl.java
@@ -1,9 +1,33 @@
 package com.gacha.model.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.stereotype.Service;
+
+import com.gacha.model.dao.UserDao;
+import com.gacha.model.dto.request.RegistRequest;
 
 @Service
 public class UserServiceImpl implements UserService {
-	
-//	throw new UserException(UserErrorCode.NOT_FOUND);
+    @Autowired
+    private UserDao userDao;
+
+    @Override
+    public boolean regist(RegistRequest registRequest) {
+        String email = registRequest.getEmail();
+        // TODO : 암호를 해시화
+        String password = registRequest.getPassword();
+        String nickname = registRequest.getNickname();
+        // TODO : 이미지 파일 S3에 올리기 -> 반환 url 저장
+        String profileImg = "";
+
+        try{
+            userDao.regist(email, password, nickname, profileImg);
+        } catch(DuplicateKeyException e){
+            System.out.println("중복된 사용자 이메일");
+            return false;
+        }
+
+        return true;
+    }
 }
