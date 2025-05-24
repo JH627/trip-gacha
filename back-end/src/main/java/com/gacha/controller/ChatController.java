26460b49cea50fdd6ebebc32919db5839472aeae
홍정훈 <2jh627@naver.com>
2025-05-25 03:48:14 +0900

    ♻️ [refactor] : swagger 명세 추가 및 검증, 예외 로직 분리

--- back-end/src/main/java/com/gacha/controller/ChatController.java
+++ back-end/src/main/java/com/gacha/controller/ChatController.java
@@ -10,6 +10,7 @@ import com.gacha.global.jwt.annotation.LoginUser;
 import com.gacha.model.dto.chat.ChatRequest.ChatDetail;
 import com.gacha.model.service.ChatService;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 
@@ -21,6 +22,8 @@ public class ChatController {
 	
     private final ChatService chatService;
 
+    @Operation(summary = "AI 채팅기능", description = "사용자가 입력한 문장을 기반으로 응답을 생성하여 반환합니다.<br/>"
+    		+ "채팅 컨텍스트는 마지막 채팅으로부터 30분간 유효하며 이후에는 사라집니다.")
     @PostMapping
     public Response<String> chat(@LoginUser Integer userId, @RequestBody ChatDetail message) {
         String response = chatService.chat(userId, message);
--- back-end/src/main/java/com/gacha/controller/ImageController.java
+++ back-end/src/main/java/com/gacha/controller/ImageController.java
@@ -12,23 +12,24 @@ import com.gacha.model.dto.enums.ImageCategory;
 import com.gacha.model.dto.image.ImageDto;
 import com.gacha.model.service.ImageService;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+
 import org.springframework.web.bind.annotation.PostMapping;
 
-
+@Tag(name = "게시판 이미지 API")
 @RestController
 @RequestMapping("/img")
+@RequiredArgsConstructor
 public class ImageController {
 
-    @Autowired
-    private ImageService imageService;
+    private final ImageService imageService;
+    private final ImageDao imageDao;
 
-    @Autowired
-    private ImageDao imageDao;
-
+    @Operation(summary = "게시판 이미지 등록 API", description = "게시판에서 사용할 이미지를 등록하고, 등록된 URL을 반환합니다.")
     @PostMapping("/board")
-    public ResponseEntity<Response<?>> uploadBoardImg(@LoginUser Integer userId, @RequestParam("img") MultipartFile file) {
+    public Response<?> uploadBoardImg(@LoginUser Integer userId, @RequestParam("img") MultipartFile file) {
         String url = imageService.upload(file, ImageCategory.board);
         
         ImageDto img = ImageDto.builder()
@@ -38,6 +39,6 @@ public class ImageController {
 
         imageDao.insert(img);
         
-        return ResponseEntity.ok(Response.onSuccess(url));
+        return Response.onSuccess(url);
     }
 }
