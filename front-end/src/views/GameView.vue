7b0c1471e84c391caf4275940709dca6b5232d77
parksangchan <gkgk00142@naver.com>
2025-05-25 12:21:52 +0900

    🐛 [fix] : FE - 게임뷰 라우팅 경로 오류 수정

--- front-end/src/views/GameView.vue
+++ front-end/src/views/GameView.vue
@@ -1,13 +1,29 @@
 <template>
   <div>
-    <button @click="show = true">게임 모달 열기</button>
-    <GameModal :is-open="show" @close="show = false" />
+    <button @click="open">게임 모달 열기</button>
+    <GameModal
+      :is-open="show"
+      @close="close"
+      :is-socket="false"
+      :invited-game-type="invitedGameType"
+    />
   </div>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue'
 import GameModal from '@/components/game/GameModal.vue'
+import { Game } from '@/components/game/Game'
 
 const show = ref(false)
+const invitedGameType = ref(Game.DEFAULT)
+
+const open = () => {
+  show.value = true
+}
+const close = () => {
+  show.value = false
+}
+
+console.log('왜안댐?')
 </script>
